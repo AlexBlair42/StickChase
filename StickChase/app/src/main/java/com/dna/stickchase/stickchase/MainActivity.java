ee68042491036210d3d8e270a830e2b3c0f35c0d
Alex Blair <ablair18@my.whitworth.edu>
2016-03-15 08:59:05 -0700

    Create Game Activity

--- StickChase/app/src/main/java/com/dna/stickchase/stickchase/MainActivity.java
+++ StickChase/app/src/main/java/com/dna/stickchase/stickchase/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonOnClick(View v){
+
+        Button button = (Button)v;
+
+        ((Button) v).setText("Clicked Numero Uno");
     }
 
     @Override
