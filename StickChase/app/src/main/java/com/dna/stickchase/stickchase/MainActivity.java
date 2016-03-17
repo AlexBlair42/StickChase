e466323b43cda38fbb157df7f399ab511b609da9
Alex Blair <ablair18@my.whitworth.edu>
2016-03-16 19:47:44 -0700

    Make button on click start new activity

--- StickChase/app/src/main/java/com/dna/stickchase/stickchase/MainActivity.java
+++ StickChase/app/src/main/java/com/dna/stickchase/stickchase/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dna.stickchase.stickchase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,18 +10,36 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static Button button_sbm;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        OnClickButtonListener();
     }
 
-    public void buttonOnClick(View v){
+    // Creates a function that makes the "New Game" button start the Game activity
 
-        Button button = (Button)v;
+    public void OnClickButtonListener(){
 
-        ((Button) v).setText("Clicked Numero Uno");
+        button_sbm  = (Button)findViewById(R.id.button);
+
+        button_sbm.setOnClickListener(
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent intent = new Intent("com.dna.stickchase.stickchase.Game");
+
+                    startActivity(intent);
+
+                }
+            }
+        );
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
